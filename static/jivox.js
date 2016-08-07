0e748ad17b3e7ef7d0a63f261b8e899c69e2ec32
ayush92garg <think.ayushgarg@gmail.com>
2016-08-07 11:01:42 +0530

    added compare feature

--- static/jivox.js
+++ static/jivox.js
@@ -12,17 +12,20 @@ var chart_data=null;
 $('.collapsible').collapsible({
 	accordion : true
 });
+
 // change chart type
-$("#bar_graph,#pie_graph").click(function(){
-	drawBarChart(chart_data,$(this).data("type"));
+$("#bar_graph,#pie_graph,#line_graph").click(function(){
+	drawBarChart(chart_data,$(this).data("type"),$(".type_select").hasClass("compare_call"));
 });
 // click of secondary navs
-$(".city,.state").click(function(){
+$(".analyze_city,.analyze_state").click(function(){
+	$(".state_select,.city_select,.type_select,.city_select_2,.city_select_1").addClass("hide");
+	$(".type_select").removeClass("compare_call");
 	// cleanup pre loaded dropdown options
 	$(".city_select select,.state_select select").find(".dynamic_list").remove();
 	// in case of city selection, show city dropdown along with type and state
 	// or show only state dropdown
-	if($(this).hasClass("city")){
+	if($(this).hasClass("analyze_city")){
 		$(".state_select,.city_select,.type_select").removeClass("hide");
 		$(".state_select").removeClass("onlyState");
 	}else{
@@ -101,11 +104,20 @@ $(document).on("change",".state_select select",function(){
 });
 // change of city of type dropdowns
 $(document).on("change",".city_select select,.type_select select",function(){
-	// empty/missing fields alert
-	if($(".city_select select").find("option:selected").attr("value")=="" && !$(".state_select").hasClass("onlyState")){
-		swal({   title: "Error!",   text: "Please select city!",   type: "error",   confirmButtonText: "Got it!" });
-		return false;
+	if($(".type_select").hasClass("compare_call")){
+		// empty/missing fields alert
+		if($(".city_select_1 select").find("option:selected").attr("value")=="" || $(".city_select_1 select").find("option:selected").attr("value")==""){
+			swal({   title: "Error!",   text: "Please select city!",   type: "error",   confirmButtonText: "Got it!" });
+			return false;
+		}
+	}else{
+		// empty/missing fields alert
+		if($(".city_select select").find("option:selected").attr("value")=="" && !$(".state_select").hasClass("onlyState")){
+			swal({   title: "Error!",   text: "Please select city!",   type: "error",   confirmButtonText: "Got it!" });
+			return false;
+		}
 	}
+	
 	// use age type as default type
 	type = $(".type_select select").find("option:selected").attr("value");
 	if($(".type_select select").find("option:selected").attr("value")==""){
@@ -113,10 +125,13 @@ $(document).on("change",".city_select select,.type_select select",function(){
 	}
 	// differect REST urls for city and states
 	if($(".state_select").hasClass("onlyState")){
-		url = "jivox/state/"+$(".state_select select").find("option:selected").attr("value")+"/"+type
+		url = "jivox/state/"+$(".state_select select").find("option:selected").attr("value")+"/"+type;
+	}else if($(".type_select").hasClass("compare_call")){
+		url ="jivox/compare/"+$(".city_select_1 select").find("option:selected").attr("value")+"/"+$(".city_select_2 select").find("option:selected").attr("value")+"/"+type;
 	}else{
 		url = "jivox/city/"+$(".state_select select").find("option:selected").attr("value")+"/"+$(".city_select select").find("option:selected").attr("value")+"/"+type;
 	}
+	console.log($(".type_select").hasClass("compare_call"));
 	$(".preloader-wrapper,.chart_sec").toggleClass("hide");
 	$.ajax({
 		"url": server_url+url,
@@ -125,18 +140,80 @@ $(document).on("change",".city_select select,.type_select select",function(){
 		"jsonpCallback": 'callback',
 		"success":function(data){
 			// draw graph on sucess
-			drawBarChart(data,"column");
+			drawBarChart(data,"column",$(".type_select").hasClass("compare_call"));
 			$(".preloader-wrapper,.chart_sec").toggleClass("hide");
 		}
 	});
 
 });
+
+// COMPARE TWO CITIES OR STATES
+$(".compare_city,.compare_state").click(function(){
+	$(".state_select").removeClass("onlyState");
+	$(".state_select,.city_select,.type_select,.city_select_2,.city_select_1").addClass("hide");
+	$(".city_select_1 select,.city_select_2 select").find(".dynamic_list").remove();
+	// Make REST call to get list of states
+	// and preload into the drop down
+	$(".preloader-wrapper,.chart_sec").toggleClass("hide");
+	$.ajax({
+		"url": server_url+"jivox/city/list",
+		"method":"GET",
+		"dataType":"jsonp",
+		"jsonpCallback": 'callback',
+		"success":function(city_list){
+			$.each(city_list,function(key,value){
+				$(".city_select_1 select,.city_select_2 select").append("<option class='dynamic_list' value="+value.cityName+">"+value.cityName+"</option>");
+			});
+			$('select').material_select();
+			$(".city_select_1,.city_select_2,.type_select").removeClass("hide");
+			$(".type_select").addClass("compare_call");
+			$(".preloader-wrapper,.chart_sec").toggleClass("hide");
+		}
+	});
+});
+$(document).on("change",".city_select_1 select,.city_select_2 select",function(){
+	// empty/missing fields alert
+	if($(".city_select_1 select").find("option:selected").attr("value")=="" || $(".city_select_2 select").find("option:selected").attr("value")==""){
+		return false;
+	}
+	if($(".city_select_1 select").find("option:selected").attr("value")==$(".city_select_2 select").find("option:selected").attr("value")){
+		swal({   title: "Error!",   text: "Please select two different cities to compare!",   type: "error",   confirmButtonText: "Got it!" });
+		return false;
+	}
+	// use age type as default type
+	type = $(".type_select select").find("option:selected").attr("value");
+	if($(".type_select select").find("option:selected").attr("value")==""){
+		type = "Age";
+	}
+	// Make REST call to get list of states
+	// and preload into the drop down
+	$(".preloader-wrapper,.chart_sec").toggleClass("hide");
+	$.ajax({
+		"url": server_url+"jivox/compare/"+$(".city_select_1 select").find("option:selected").attr("value")+"/"+$(".city_select_2 select").find("option:selected").attr("value")+"/"+type,
+		"method":"GET",
+		"dataType":"jsonp",
+		"jsonpCallback": 'callback',
+		"success":function(data){
+			drawBarChart(data,"column",true);
+			$(".preloader-wrapper,.chart_sec").toggleClass("hide");
+		}
+	});
+});
 // draw graph
 // uses highcharts api to draw charts
-function drawBarChart(data,chart_type){
+function drawBarChart(data,chart_type,multi_graph){
 	chart_data = data;
-	$("#bar_graph,#pie_graph").removeClass("hide");
-	arr = $.map(data[1], function(el) { return el });
+	$("#bar_graph,#pie_graph,#line_graph").removeClass("hide");
+	if(typeof multi_graph == 'undefined' || !multi_graph){
+		arr = $.map(data[1], function(el) { return el });
+		series_arr = [{
+			"name":"Population",
+			data:arr
+		}];
+	}else{
+		series_arr = data[1];
+		$("#pie_graph").addClass("hide");
+	}
 	$('#container').highcharts({
 		chart: {
 			type: chart_type
@@ -153,11 +230,16 @@ function drawBarChart(data,chart_type){
 		plotOptions: {
 			series: {
 			// stacking: 'normal'
+			},
+			"pie":{
+				allowPointSelect: false,
+				cursor: 'pointer',
+				showInLegend: false,
+				dataLabels: {
+					enabled: false,
+				}
 			}
 		},
-		series: [{
-			"name":"Population",
-			data:arr
-		}]
+		series: series_arr
 	});
 }
